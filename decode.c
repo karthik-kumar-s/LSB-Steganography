1ff9fbd119ce215ed5f6737d4b39c55973b21880
Karthik Kumar S <karthikkumarece001@gmail.com>
2025-12-21 13:05:58 +0530

    Open stego image and skip BMP header

--- decode.c
+++ decode.c
@@ -1,13 +1,19 @@
 #include "decode.h"
 
-// Main decoding controller 
+// Main decoding controller
 Status do_decoding(DecodeInfo *decInfo)
 {
-    // This will be implemented step by step
+    // Open stego image
+    if (open_files(decInfo) == e_failure)
+        return e_failure;
+
+    // Skip BMP header (first 54 bytes)
+    fseek(decInfo->fptr_stego_image, BMP_HEADER_SIZE, SEEK_SET);
+
     return e_success;
 }
 
-// Open stego image file 
+// Open stego image file
 Status open_files(DecodeInfo *decInfo)
 {
     decInfo->fptr_stego_image = fopen(decInfo->stego_image_fname, "rb");
@@ -17,5 +23,36 @@ Status open_files(DecodeInfo *decInfo)
         perror("fopen");
         return e_failure;
     }
+    return e_success;
+}
+// Decode one byte from 8 image bytes
+Status decode_lsb_to_byte(char *data, FILE *fptr)
+{
+    char image_byte;
+    *data = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        fread(&image_byte, 1, 1, fptr);          // Read one image byte
+        *data = (*data << 1) | (image_byte & 1); // Collect LSB
+    }
+    return e_success;
+}
+
+// Decode and verify magic string
+Status decode_magic_string(DecodeInfo *decInfo)
+{
+    char magic[strlen(MAGIC_STRING) + 1];
+
+    for (int i = 0; i < strlen(MAGIC_STRING); i++)
+    {
+        decode_lsb_to_byte(&magic[i], decInfo->fptr_stego_image);
+    }
+
+    magic[strlen(MAGIC_STRING)] = '\0';
+
+    if (strcmp(magic, MAGIC_STRING) != 0)
+        return e_failure;
+
     return e_success;
 }
